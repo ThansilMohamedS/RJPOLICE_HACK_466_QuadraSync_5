4258c18cd259f81fb9e9b4e2f01c9d820823b4ac
ThansilMohamedS <110482902+ThansilMohamedS@users.noreply.github.com>
2024-01-18 09:39:05 +0530

    Add files via upload

---
+++ advertisement image processing.py
@@ -0,0 +1,42 @@
+import pytesseract
+from PIL import Image
+from googlesearch import search
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR'  # Change the path accordingly
+
+def ocr(image_path):
+    try:
+        image = Image.open(image_path)
+        text = pytesseract.image_to_string(image)
+        return text.strip()
+    except Exception as e:
+        print(f"Error in OCR function: {e}")
+        return None
+
+def search_text(text):
+    try:
+        search_results = list(search(text, num=4, stop=4))
+        return search_results
+    except Exception as e:
+        print(f"Error in search_text function: {e}")
+        return None
+
+def is_text_present_in_search(image_path):
+    extracted_text = ocr(image_path)
+    if extracted_text is None:
+        return False
+
+    search_results = search_text(extracted_text)
+    if search_results is None:
+        return False
+
+    # Check if the extracted text is present in the top 4 search results
+    if any(extracted_text.lower() in result.lower() for result in search_results):
+        return True
+    else:
+        return False
+
+image_path = r'C:\Users\Thansil\Desktop\Python\dominoz.png'
+if is_text_present_in_search(image_path):
+    print("The image is true.")
+else:
+    print("The image is not true.")
